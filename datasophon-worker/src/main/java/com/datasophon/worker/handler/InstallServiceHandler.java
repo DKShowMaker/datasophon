58c9ad76ebd43b003fbd967fa116aa7b5334caf1
DKShowMaker <zhangtr1@qq.com>
2026-03-12 21:18:18 +0800

    fix: 前端查看安装日志会看到worker上对相应服务执行命令日志，现在将安装日志和控制日志统一。

--- datasophon-worker/src/main/java/com/datasophon/worker/handler/InstallServiceHandler.java
+++ datasophon-worker/src/main/java/com/datasophon/worker/handler/InstallServiceHandler.java
@@ -72,7 +72,7 @@ public class InstallServiceHandler {
         this.frameCode = frameCode;
         this.serviceName = serviceName;
         this.serviceRoleName = serviceRoleName;
-        String loggerName = String.format("%s-%s-%s-%s", TaskConstants.TASK_LOG_LOGGER_NAME, frameCode, serviceName,
+        String loggerName = String.format("%s-%s-%s", TaskConstants.TASK_LOG_LOGGER_NAME, serviceName,
                 serviceRoleName);
         logger = LoggerFactory.getLogger(loggerName);
     }
